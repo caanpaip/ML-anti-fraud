9364d8178316619d7c134de07549237e6210de31
caanpaip <professor.palechor@gmail.com>
2022-11-16 21:10:06 -0300

    atualizando o requirements.py

--- src/utils/parameters.py
+++ src/utils/parameters.py
@@ -3,7 +3,7 @@ import sys
 ## Parameters
 if sys.platform=='win32':
     ## path in windows machine
-    root = "c:\\Users\\caanp\\OneDrive\\Documents\\repositories\\ML-ANTI-FRAUD"
+    root = "c:\\Users\\caanp\\OneDrive\\Documents\\repositories\\ML-anti-fraud"
 
 elif sys.platform=='linux':
     ## path in Linux machine
